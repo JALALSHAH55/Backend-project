e258ddd713468e0bd0b30f0cf5ef14ccd020144b
JALAL SHAH <149874926+JALALSHAH55@users.noreply.github.com>
2024-02-01 21:24:05 +0500

    login api

--- Routes/customerRoutes.js
+++ Routes/customerRoutes.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 const customerController  = require('../controller/customerController');
 
-router.post('/customers',customerController.signUp)
+router.post('/customers',customerController.signUp);
+router.post('/customers',customerController.login);
 
 // router.route('/customers').get(customerController.get);
 
--- controller/customerController.js
+++ controller/customerController.js
@@ -1,4 +1,5 @@
 const {customers: Customers} = require('../models');
+const bcrypt = require('bcryptjs');
 const {Op} = require("sequelize");
 module.exports = {
     signUp: async function (req, res) {
@@ -16,10 +17,45 @@ module.exports = {
             if(customer){
                 return res.status(400).send('Customer already exists with this email');
             }
+            const hashPassword = await bcrypt.hash(password, 12);
              customer = await Customers.create({
                 name,
                 email,
-                password,
+                password: hashPassword,
+            })
+             res.status(201).send({
+                message: "customer added succesfully",
+                customer,
+             });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err.message || 'something went wrong')
+        }
+    },
+
+    login: async function (req, res) {
+        try {
+            const { email, password } = req.body;
+            if( !email || !password){
+                return res.status(409).send('Required field cammot be empty');
+            }
+
+            let customer = await Customers.findOne({
+                where:{
+                    email,
+                }
+            })
+            if(!customer){
+                return res.status(404).send('Customer not found');
+            }
+            const comparePassword = await bcrypt.compare(password, customer.password);
+            if(!comparePassword){
+                return res.status().send
+            }
+             customer = await Customers.create({
+                name,
+                email,
+                password: hashPassword,
             })
              res.status(201).send({
                 message: "customer added succesfully",
